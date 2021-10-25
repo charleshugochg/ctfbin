f2efc0c213fff16fbe1c88fcb68dbe157872374d
charleshugochg <minheinkhant@hotmail.com>
2021-10-25 23:09:20 +0630

    navbar added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
+import Navbar from './components/Navbar/Navbar';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React!</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ frontend/src/components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import classes from './style.module.css'
+
+const Navbar = (props) => {
+  return (
+    <div className={classes.container}>
+      <h2>ctfb1n</h2>
+    </div>
+  );
+}
+
+export default Navbar
