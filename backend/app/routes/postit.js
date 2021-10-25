37f28ab286e214d2173447bcf0060ac22d187de5
charleshugochg <minheinkhant@hotmail.com>
2021-10-25 17:46:07 +0630

    change postit api

--- backend/app/routes/postit.js
+++ backend/app/routes/postit.js
@@ -4,13 +4,13 @@ const router = express.Router()
 let postit = ''
 
 router.get('/', async (req, res) => {
-  const data = req.query.data
-  if (data === undefined) {
-    return res.send(postit)
-  } else {
-    postit = data
-    return res.send(postit)
-  }
+  return res.send(postit)
+})
+
+router.get('/set/*', async (req, res) => {
+  const data = req.path.slice(5)
+  postit = data
+  return res.send(postit)
 })
 
 module.exports = {
