6592f3435baa63dd69d564c85857e7d69f4227b4
charleshugochg <minheinkhant@hotmail.com>
2021-12-14 13:31:54 +0630

    update link match to match non exact path

--- frontend/src/components/LinkMatch/LinkMatch.js
+++ frontend/src/components/LinkMatch/LinkMatch.js
@@ -1,8 +1,8 @@
 import { Link, useMatch, useResolvedPath } from 'react-router-dom'
 
-export const LinkMatch = ({ to, ...rest }) => {
+export const LinkMatch = ({ to, exact = false, ...rest }) => {
   const resolved = useResolvedPath(to)
-  const match = useMatch({ path: resolved.pathname, end: true })
+  const match = useMatch({ path: resolved.pathname, end: exact})
 
   let {children} = rest
   if (typeof children === 'function')
