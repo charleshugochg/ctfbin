014c0920b3a826857e6b56de8c8b33126656c2be
charleshugochg <minheinkhant@hotmail.com>
2021-12-18 14:35:53 +0630

    login screen control variable is added

--- frontend/src/screens/LoginScreen/LoginScreen.js
+++ frontend/src/screens/LoginScreen/LoginScreen.js
@@ -1,15 +1,27 @@
+import { useState } from 'react'
 import classes from './loginscreen.module.css'
 
 import EditText from '../../components/EditText/EditText'
 import Button from '../../components/Button/Button'
 
 export const LoginScreen = (props) => {
+  const [pwd, setPwd] = useState('')
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault()
+    console.log('submitted with pwd', pwd)
+  }
+
+  const handleTextChange = (e) => {
+    setPwd(() => e.target.value)
+  }
+
   return (
     <div className={classes.container}>
-      <div className={classes.form}>
-        <EditText placeholder="Enter your password" type="password" />
-        <Button text="login" />
-      </div>
+      <form className={classes.form} onSubmit={handleFormSubmit}>
+        <EditText value={pwd} placeholder="Enter your password" type="password" onChange={handleTextChange} />
+        <Button text="login" type="submit"/>
+      </form>
     </div>
   )
 }
