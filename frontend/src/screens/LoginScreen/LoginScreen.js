d2783c2653ed277329c73e847de5e41364d41c1e
charleshugochg <minheinkhant@hotmail.com>
2021-12-18 14:29:56 +0630

    layout the button and the field in the login screen

--- frontend/src/screens/LoginScreen/LoginScreen.js
+++ frontend/src/screens/LoginScreen/LoginScreen.js
@@ -1,12 +1,14 @@
 import classes from './loginscreen.module.css'
 
 import EditText from '../../components/EditText/EditText'
+import Button from '../../components/Button/Button'
 
 export const LoginScreen = (props) => {
   return (
     <div className={classes.container}>
-      <div className={classes['password-field']}>
+      <div className={classes.form}>
         <EditText placeholder="Enter your password" type="password" />
+        <Button text="login" />
       </div>
     </div>
   )
